2e33f3cf353f3cd64a9aaae66ab7b3e799800a0e
dison  ruan <disonruan@disons-MacBook-Air.local>
2017-02-28 17:34:04 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,26 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = set_student
+    #binding.pry
   end
 
+  def activate
+    @students = set_student
+    @student.activity_swap
+    @student.save
+    redirect_to "/students/#{@student.id}"
+  end
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activity
+    return "This student is currently active." if self.active
+    "This student is currently inactive."
+  end
+
+  def activity_swap
+    self.active ? self.active = false : self.active = true
+  end
 end
